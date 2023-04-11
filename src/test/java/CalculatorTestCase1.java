8d447526c6eaf74af14514b1d181dd602021733d
anetochkadmi <anyamyliferulitforever@gmail.com>
2023-04-11 17:41:49 +0200

    add test case 2

--- src/test/java/CalculatorTestCase1.java
+++ src/test/java/CalculatorTestCase1.java
@@ -3,7 +3,7 @@ import org.cvetok39.calculator.core.CalculatorPack;
 import org.cvetok39.calculator.core.CalculatorParams;
 import org.cvetok39.calculator.tests.CalculatorAddFlowerAndCompareInCartTest;
 
-public class CalculatorTestCases {
+public class CalculatorTestCase1 {
     public static void main(String[] args) {
         CalculatorParams params = new CalculatorParams();
 
